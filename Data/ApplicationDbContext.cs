d9ad799037c22ea76d674540b03319cca4c8d651
batarilanedbryle-cloud <batarilanedbryle@gmail.com>
2026-05-20 14:05:38 +0800

    Add files via upload

---
+++ Data/ApplicationDbContext.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Batarilan_Exercise1.Models;
+
+namespace Batarilan_Exercise1.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<SitIn> SitIns { get; set; }
+
+        public DbSet<UserAccount> Users { get; set; }
+        public DbSet<Announcement> Announcements { get; set; }
+        public DbSet<Reward> Rewards { get; set; }
+        public DbSet<Reservation> Reservations { get; set; }
+        public DbSet<LabHistory> LabHistories { get; set; }
+        public DbSet<Feedback> Feedbacks { get; set; }
+        public DbSet<Batarilan_Exercise1.Models.PcUnit> PcUnits { get; set; }
+
+    }
+}
---
+++ Data/FakeDatabase.cs
@@ -0,0 +1,22 @@
+﻿using Batarilan_Exercise1.Models;
+using System.Collections.Generic;
+
+namespace Batarilan_Exercise1.Data
+{
+    public static class FakeDatabase
+    {
+        public static List<UserAccount> Users = new List<UserAccount>()
+        {
+            new UserAccount
+            {
+                Id = 1,
+                IdNumber = "ADMIN001",
+                FirstName = "System",
+                LastName = "Administrator",
+                Email = "admin@ccs.com",
+                Password = "admin123",
+                Role = "Admin"
+            }
+        };
+    }
+}
